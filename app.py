5d1b1e57c7b5105970d7c836a66a690518d0ce75
caioherobr <caioherorj@gmail.com>
2025-11-25 08:47:19 -0300

    att tokens pt 1
    att tokens parte 1 colocando a funcionalidade suportada para mensalidade agora faltando só a assinatura da stripe para funcionar

--- app.py
+++ app.py
@@ -828,5 +828,5 @@ def stripe_webhook():
 # FIM DAS NOVAS ROTAS STRIPE 
 # ------------------------------------------------------------------------
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
